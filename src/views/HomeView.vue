7af8ace764c80add290064bae3ef641030bf9cc0
JiahuiFF <jiahui.code@gmail.com>
2024-06-11 16:12:24 +0800

    feat: cube

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,49 @@
 <script setup>
+import { onMounted, ref } from 'vue'
+
+// 导入Three.js
+import * as THREE from 'three'
+
+const refContent = ref(null)
+
+onMounted(() => {
+  // 创建场景
+  const scene = new THREE.Scene()
+
+  // 初始化相机
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000,
+  )
+
+  // 初始化渲染器
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+
+  // 使用ref作为Three.js场景的挂载点
+  refContent.value && refContent.value.appendChild(renderer.domElement)
+
+  // 初始化立方体模型
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshBasicMaterial({ color: 0x00FF00 })
+  const cube = new THREE.Mesh(geometry, material)
+  scene.add(cube)
+  camera.position.z = 5
+
+  const animate = function () {
+    requestAnimationFrame(animate)
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+    renderer.render(scene, camera)
+  }
+  animate()
+})
 </script>
 
 <template>
   <main>
-    Hello, world!  test
+    <div ref="refContent" />
   </main>
 </template>
